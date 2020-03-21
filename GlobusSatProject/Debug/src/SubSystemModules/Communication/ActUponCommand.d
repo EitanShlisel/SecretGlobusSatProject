857a6260feade4379bbd4b212d6681c60929a717
Adi Rabinovitz <adiwwww@gmail.com>
2020-03-21 20:05:13 +0200

    changing configuration

--- GlobusSatProject/Debug/src/SubSystemModules/Communication/ActUponCommand.d
+++ GlobusSatProject/Debug/src/SubSystemModules/Communication/ActUponCommand.d
@@ -1,40 +1,40 @@
 src/SubSystemModules/Communication/ActUponCommand.d: \
  ../src/SubSystemModules/Communication/ActUponCommand.c \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h \
  ../src/SubSystemModules/Communication/TRXVU.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/GlobalStandards.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SysI2CAddr.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TestingConfigurations.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/GlobalStandards.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SysI2CAddr.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/TestingConfigurations.h \
  ../src/SubSystemModules/Communication/AckHandler.h \
  ../src/SubSystemModules/Communication/SatCommandHandler.h \
  ../src/SubSystemModules/Communication/SPL.h \
  ../src/SubSystemModules/Communication/ActUponCommand.h \
  ../src/SubSystemModules/Communication/CommandDictionary.h
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h:
 
 ../src/SubSystemModules/Communication/TRXVU.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/GlobalStandards.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/GlobalStandards.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SysI2CAddr.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SysI2CAddr.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TestingConfigurations.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/TestingConfigurations.h:
 
 ../src/SubSystemModules/Communication/AckHandler.h:
 
--- GlobusSatProject/Debug/src/SubSystemModules/Communication/TRXVU.d
+++ GlobusSatProject/Debug/src/SubSystemModules/Communication/TRXVU.d
@@ -1,104 +1,104 @@
 src/SubSystemModules/Communication/TRXVU.d: \
  ../src/SubSystemModules/Communication/TRXVU.c \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOS.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/projdefs.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOSConfig.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/trace.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/dbgu/dbgu.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/pio/pio.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/assert.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/mpu_wrappers.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/semphr.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/queue.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/task.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/list.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Timing/Time.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisAntS.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/GlobalStandards.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SysI2CAddr.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TestingConfigurations.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOS.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/projdefs.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOSConfig.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/trace.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/dbgu/dbgu.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/pio/pio.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/assert.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/mpu_wrappers.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/semphr.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/queue.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/task.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/list.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Timing/Time.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisAntS.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/GlobalStandards.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SysI2CAddr.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/TestingConfigurations.h \
  ../src/SubSystemModules/Communication/TRXVU.h \
  ../src/SubSystemModules/Communication/AckHandler.h \
  ../src/SubSystemModules/Communication/SatCommandHandler.h \
  ../src/SubSystemModules/Communication/SPL.h \
  ../src/SubSystemModules/Communication/ActUponCommand.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TLM_management.h \
- C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Boolean.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPS.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPSOperationModes.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Maintenance/Maintenance.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryCollector.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryFiles.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatCommandHandler.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatDataTx.h \
- C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/Beacon.h
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/TLM_management.h \
+ C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Boolean.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPS.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPSOperationModes.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Maintenance/Maintenance.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryCollector.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryFiles.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatCommandHandler.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatDataTx.h \
+ C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/Beacon.h
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOS.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOS.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/projdefs.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/projdefs.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOSConfig.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/FreeRTOSConfig.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/board.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/boards/ISIS_OBC_G20/at91sam9g20/AT91SAM9G20.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/trace.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/trace.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/dbgu/dbgu.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/dbgu/dbgu.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/pio/pio.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/peripherals/pio/pio.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/assert.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/assert.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/GCC/ARM9_AT91SAM9G20/portmacro.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/portable/MemMang/standardMemMang.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/mpu_wrappers.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/mpu_wrappers.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/semphr.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/semphr.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/queue.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/queue.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/task.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/task.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/list.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/freertos/include/freertos/list.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Timing/Time.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Timing/Time.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/boolean.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/errors.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisTRXVU.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisAntS.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//satellite-subsystems/satellite-subsystems/include/satellite-subsystems/IsisAntS.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/GlobalStandards.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/GlobalStandards.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/at91/include/at91/utility/exithandler.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/FRAM_FlightParameters.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Storage/FRAM.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SysI2CAddr.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SysI2CAddr.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TestingConfigurations.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/TestingConfigurations.h:
 
 ../src/SubSystemModules/Communication/TRXVU.h:
 
@@ -110,22 +110,22 @@ C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TestingCon
 
 ../src/SubSystemModules/Communication/ActUponCommand.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/TLM_management.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/TLM_management.h:
 
-C:/Users/97250/Documents/GitHub/GlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Boolean.h:
+C:/ISIS/workspace/SecretGlobusSatProject/GlobusSatProject/..//hal/hal/include/hal/Boolean.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPS.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPS.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPSOperationModes.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/PowerManagment/EPSOperationModes.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Maintenance/Maintenance.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Maintenance/Maintenance.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryCollector.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryCollector.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryFiles.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Housekepping/TelemetryFiles.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatCommandHandler.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatCommandHandler.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatDataTx.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/SatDataTx.h:
 
-C:\Users\97250\Documents\GitHub\GlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/Beacon.h:
+C:\ISIS\workspace\SecretGlobusSatProject\GlobusSatProject\src/SubSystemModules/Communication/Beacon.h:
