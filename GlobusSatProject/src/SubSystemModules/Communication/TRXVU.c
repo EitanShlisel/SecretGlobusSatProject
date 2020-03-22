c5e558b8a40b22c7a77c0feb58161f01a494b9d5
ginger123 <arielabr.271098@gmail.com>
2020-03-22 23:44:40 +0200

    add dump(untested - may not work)

--- GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
+++ GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
@@ -127,6 +127,7 @@ void FinishDump(dump_arguments_t *task_args,unsigned char *buffer, ack_subtype_t
 	}
 	if (xDumpHandle != NULL) {
 		vTaskDelete(xDumpHandle);
+		xDumpHandle = NULL;
 	}
 	if(NULL != buffer){
 		free(buffer);
@@ -157,11 +158,23 @@ void SendDumpAbortRequest() {
 Boolean CheckDumpAbort() {
 	portBASE_TYPE err;
 	Boolean queue_msg;
-	err = xQueueReceive(xDumpQueue, &queue_msg, SECONDS_TO_TICKS(1));
+	err = xQueueReceive(xDumpQueue, &queue_msg, 0);
 	if (err == pdPASS) {
 		return queue_msg;
 	}
 	return FALSE;
+}
+
+int getTelemetryMetaData(tlm_type type, char* filename, int* size_of_element) {
+	int err = 0;
+	err = GetTelemetryFilenameByType(type, filename);
+	if (0 != err) {
+		return err;
+	}
+	if(c_fileGetSizeOfElement(filename,size_of_element)!=TRUE) {
+		return -1;
+	}
+	return err;
 }
 
 void DumpTask(void *args) {
@@ -173,41 +186,66 @@ void DumpTask(void *args) {
 	sat_packet_t dump_tlm = { 0 };
 
 	int err = 0;
-	int availFrames = 0;
+	Boolean is_last_read = FALSE;
+	FileSystemResult result = 0;
+	int availFrames = 1;
+	int num_packets_read = 0; //number of packets read from buffer (single_time)
+	int total_packets_read = 0; //total number of packets read from buffer
 	unsigned int num_of_elements = 0;
 	unsigned int size_of_element = 0;
+	unsigned int size_of_element_with_timestamp;
+	time_unix last_read_time; // this is the last time we have on the buffer
+	time_unix last_sent_time = task_args->t_start; // this is the last we actually sent(where we want to search next)
 
 	unsigned char *buffer = NULL;
 	char filename[MAX_F_FILE_NAME_SIZE] = { 0 };
 
-	err = GetTelemetryFilenameByType((tlm_type) task_args->dump_type,
-			filename);
-	if (0 != err) {
+	buffer = malloc(SIZE_DUMP_BUFFER);
+
+	err = getTelemetryMetaData(task_args->dump_type, filename, &size_of_element);
+	size_of_element_with_timestamp = size_of_element + sizeof(time_unix);
+	if(0 != err) {
 		FinishDump(task_args, buffer, ACK_DUMP_ABORT, (unsigned char*) &err,sizeof(err));
 		return;
 	}
 
+	// TODO: consider if we actually want to know the number of packets that will be sent,
+	// as it won't be exactly easy.
 	SendAckPacket(ACK_DUMP_START, task_args->cmd,
 			(unsigned char*) &num_of_elements, sizeof(num_of_elements));
 
-	for (unsigned int i = 0; i < num_of_elements; i++) {
+	while(!is_last_read) {
+		// read
+		num_packets_read = 0;
 
-		if (CheckDumpAbort() || CheckTransmitionAllowed()) {
-			FinishDump(task_args, buffer, ACK_DUMP_ABORT, NULL, 0);
-		}
-		if (0 == availFrames) {
-			vTaskDelay(10);
+		result = c_fileRead(filename, buffer, SIZE_DUMP_BUFFER, 
+		last_sent_time, task_args->t_end, &num_packets_read, &last_read_time);
+
+		if(FS_SUCCSESS == result) {
+			is_last_read = TRUE;
 		}
 
-		AssembleCommand((unsigned char*)buffer, size_of_element,
+		last_sent_time = last_read_time;
+		total_packets_read += num_packets_read;
+		// send packets
+		for(int i = 0; i < num_packets_read; i++) {
+			if (CheckDumpAbort() || CheckTransmitionAllowed()) {
+				FinishDump(task_args, buffer, ACK_DUMP_ABORT, NULL, 0);
+				return;
+			}
+			if (0 == availFrames) {
+				vTaskDelay(10);
+			}
+			AssembleCommand((unsigned char*)buffer + size_of_element_with_timestamp * i, 
+				size_of_element_with_timestamp,
 				(char) DUMP_SUBTYPE, (char) (task_args->dump_type),
 				task_args->cmd->ID, &dump_tlm);
-
-		//TODO: finish dump
-
-		TransmitSplPacket(&dump_tlm, &availFrames);
+			TransmitSplPacket(&dump_tlm, &availFrames);
+		}
 	}
+		
 	FinishDump(task_args, buffer, ACK_DUMP_FINISHED, NULL, 0);
+
 }
 
 int DumpTelemetry(sat_packet_t *cmd) {
@@ -219,6 +257,9 @@ int DumpTelemetry(sat_packet_t *cmd) {
 	unsigned int offset = 0;
 
 	dmp_pckt->cmd = cmd;
+	dmp_pckt->dump_type = ((dump_arguments_t*)cmd->data)->dump_type;
+	dmp_pckt->t_start = ((dump_arguments_t*)cmd->data)->t_start;
+	dmp_pckt->t_end = ((dump_arguments_t*)cmd->data)->t_end;
 
 	memcpy(&dmp_pckt->dump_type, cmd->data, sizeof(dmp_pckt->dump_type));
 	offset += sizeof(dmp_pckt->dump_type);
