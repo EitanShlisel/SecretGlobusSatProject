a839d7ee230e41160278fd08b7ed65f2f1522481
EitanSh <eitan.shlisel@gmail.com>
2020-06-18 23:03:11 +0300

    eitan

--- GlobusSatProject/src/SubSystemModules/Maintenance/SatMacros.c
+++ GlobusSatProject/src/SubSystemModules/Maintenance/SatMacros.c
@@ -1,8 +1,19 @@
-/*
- * SatMacros.c
- *
- *  Created on: Mar 5, 2020
- *      Author: Eitan
- */
+#include "SatMacros.h"
+#include "hal/boolean.h"
+
+Boolean8bit macro_write_enabled[MACRO_NUM_OF_MACROS] = {0};
 
 
+macro_err MacroExecuteMacro(MacroTypes macro_type,int index){
+	return 0;
+}
+
+macro_err MacroAllowEdit(MacroTypes macro_type,int index){
+	return 0;
+}
+
+macro_err MacroEditMacro(MacroTypes macro_type,int index,
+		sat_packet_t *macro_cmd,unsigned int length){
+
+	return 0;
+}
